f8b5c3f19ab07715ef687bdce43a11ee5a4a5f3d
Gadzhi Kharkharov <me@kkga.me>
2022-01-19 02:55:35 +0300

    add filesystem walk

--- main.ts
+++ main.ts
@@ -1,122 +1,104 @@
-/* Section: Dependencies */
+/* Dependencies */
 
 import { Marked } from "https://deno.land/x/markdown@v2.0.0/mod.ts";
 import { ensureFileSync } from "https://deno.land/std@0.121.0/fs/mod.ts";
+import { walkSync } from "https://deno.land/std@0.121.0/fs/mod.ts";
+import { slugify } from "https://deno.land/x/slugify/mod.ts";
 
-/* Section: Constants */
+/* Constants */
 
-const FIRST_ITEM_INDEX = 0;
-const SECOND_ITEM_INDEX = 1;
-const COMPONENT_DELIMITER = "+++";
-const COMPONENT_TYPE_PATTERN = /<\S>(.*?)\:(.*?)<\/\S>/g;
-const HTML_CONTENT_PATTERN = /\n(.*)/gs;
-const LAYOUT_PREFIX = "layout";
-const HOME_PATH = "/home";
+const INDEX_PATH = "index";
 const STYLESHEET_PATH = "styles.css";
 
-/* Section: Interfaces and Globals */
+/* Interfaces and Globals */
 
 interface Page {
   path: string;
-  name: string;
+  title: string;
   html: string;
 }
 
-interface Layout {
-  [key: string]: any;
-}
-
 let pages: Array<Page> = [];
-let layout: Layout = {};
+
+/* -------------------------- */
 
 /* Step 0: Grab CLI arguments */
 
-const filename = Deno.args[FIRST_ITEM_INDEX];
-const buildPath = Deno.args[SECOND_ITEM_INDEX] || "./build";
+const contentPath = Deno.args[0] || ".";
+const buildPath = Deno.args[1] || "./build";
 
-if (!filename) {
-  console.log("Please specify .md file");
-  Deno.exit(1);
-} else {
-  console.log(`Building site with '${filename}' into '${buildPath}'`);
+// if (!filename) {
+//   console.log("Please specify .md file");
+//   Deno.exit(1);
+// } else {
+//   console.log(`Building site with '${filename}' into '${buildPath}'`);
+// }
+
+/* Step 1: Read files */
+
+let paths: Array<string> = [];
+const decoder = new TextDecoder("utf-8");
+
+for (const entry of walkSync(contentPath)) {
+  if (entry.isFile && entry.name.endsWith(".md")) {
+    paths.push(entry.path);
+  }
 }
 
-/* Step 1: Parse metadata and components from markdown file */
+/* Step 2: Construct page data */
 
-const decoder = new TextDecoder("utf-8");
-const fileContent = decoder.decode(Deno.readFileSync(filename));
-
-const components = fileContent.split(COMPONENT_DELIMITER);
-const { meta: frontMatter } = Marked.parse(components[FIRST_ITEM_INDEX]);
-const { title, styles, favicon } = frontMatter;
-// console.log({ title, styles, favicon });
-
-/* Step 2: Construct page data from components */
-
-for (const component of components) {
-  const { content } = Marked.parse(component);
-
-  const [matchedComponentType] = content.matchAll(COMPONENT_TYPE_PATTERN);
-  const [, path, name] = matchedComponentType;
-
-  const [matchedHtml] = content.matchAll(HTML_CONTENT_PATTERN);
-  const [, html] = matchedHtml;
-
-  const isLayoutComponent = path === LAYOUT_PREFIX;
-
-  if (isLayoutComponent) {
-    layout[name] = html;
-  } else {
-    pages.push({ path, name, html });
-  }
+for (const path of paths) {
+  const fileContent = decoder.decode(Deno.readFileSync(path));
+  const { meta: frontMatter, content } = Marked.parse(fileContent);
+  const { title } = frontMatter;
+  const slug = slugify(title, { lower: true });
+  pages.push({ path: slug, title, html: content });
 }
 
 /* Step 3: Generate templates for html content */
 
-const isHomePath = (path: string) => path === HOME_PATH;
+// const isHomePath = (path: string) => path === HOME_PATH;
 
-const getStylesheetHref = (path: string) => {
-  return isHomePath(path) ? STYLESHEET_PATH : `../${STYLESHEET_PATH}`;
-};
+// const getStylesheetHref = (path: string) => {
+//   return isHomePath(path) ? STYLESHEET_PATH : `../${STYLESHEET_PATH}`;
+// };
 
-const getFaviconSvg = (favicon: string) => `
-  <svg xmlns="http://www.w3.org/2000/svg">
-    <text y="32" font-size="32">${favicon ? favicon : "🦕"}</text>
-  </svg>
-`;
+// const getFaviconSvg = (favicon: string) => `
+//   <svg xmlns="http://www.w3.org/2000/svg">
+//     <text y="32" font-size="32">${favicon ? favicon : "🦕"}</text>
+//   </svg>
+// `;
 
-const getNavigation = (currentPath: string) => `
-  <div id="nav">
-    ${
-  pages.map(({ path, name }) => {
-    const href = path === HOME_PATH ? "/" : path;
-    const isSelectedPage = path === currentPath;
-    const classes = `nav-item ${isSelectedPage ? "selected" : ""}`;
-    return `<a class="${classes}" href=${href}>${name}</a>`;
-  }).join("\n")
-}
-  </div>
-`;
+// const getNavigation = (currentPath: string) => `
+//   <div id="nav">
+//     ${
+//   pages.map(({ path, name }) => {
+//     const href = path === HOME_PATH ? "/" : path;
+//     const isSelectedPage = path === currentPath;
+//     const classes = `nav-item ${isSelectedPage ? "selected" : ""}`;
+//     return `<a class="${classes}" href=${href}>${name}</a>`;
+//   }).join("\n")
+// }
+//   </div>
+// `;
 
-const footer = layout.footer ? `<div id="footer">${layout.footer}</div>` : "";
+// const footer = layout.footer ? `<div id="footer">${layout.footer}</div>` : "";
 
-const getHtmlByPage = ({ path, name, html }: Page) => `
+const getHtmlByPage = ({ path, title, html }: Page) => `
 <!DOCTYPE html>
 <html>
 <head>
-<title>${name} | ${title}</title>
-<link rel="stylesheet" href="${getStylesheetHref(path)}">
-<link rel="icon" href="/favicon.svg">
+  <title>${title}</title>
+  <link rel="stylesheet" href="${""}">
+  <link rel="icon" href="/favicon.svg">
 </head>
   <body>
     <div id="title">
       ${title}
     </div>
-    ${getNavigation(path)}
     <div id="main">
       ${html}
     </div>
-    ${footer}
   </body>
 </html>
 `;
@@ -125,19 +107,20 @@ const getHtmlByPage = ({ path, name, html }: Page) => `
 
 for (const page of pages) {
   const { path } = page;
+  console.log(path)
 
   let outputPath: string;
 
-  if (path === HOME_PATH) {
+  if (path === INDEX_PATH) {
     outputPath = `${buildPath}/index.html`;
   } else {
-    outputPath = `${buildPath}${path}/index.html`;
+    outputPath = `${buildPath}/${path}/index.html`;
   }
 
   ensureFileSync(outputPath);
   Deno.writeTextFileSync(outputPath, getHtmlByPage(page));
 }
 
-/* Step 5: Build additional asset files */
-Deno.writeTextFileSync(`${buildPath}/styles.css`, styles ? styles : "");
-Deno.writeTextFileSync(`${buildPath}/favicon.svg`, getFaviconSvg(favicon));
+// /* Step 5: Build additional asset files */
+// Deno.writeTextFileSync(`${buildPath}/styles.css`, styles ? styles : "");
+// Deno.writeTextFileSync(`${buildPath}/favicon.svg`, getFaviconSvg(favicon));
