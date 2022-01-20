669c4634fe3803bb57d6bef514f72472e93abe04
Gadzhi Kharkharov <me@kkga.me>
2022-01-20 13:00:11 +0300

    upd

--- main.ts
+++ main.ts
@@ -109,35 +109,33 @@ const getHtmlByPage = ({ meta, html }: Page) => {
   return `
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <title>${title}</title>
-  <link href="https://unpkg.com/@primer/css@^16.0.0/dist/primer.css" rel="stylesheet" />
-  <link rel="icon" href="/favicon.svg">
-  <style>
-  	.markdown-body {
-  		box-sizing: border-box;
-  		min-width: 200px;
-  		max-width: 980px;
-  		margin: 0 auto;
-  		padding: 45px;
-  	}
-
-  	@media (max-width: 767px) {
-  		.markdown-body {
-  			padding: 15px;
-  		}
-  	}
-  </style>
-</head>
+  <head>
+    <title>${title}</title>
+    <link href="https://unpkg.com/@primer/css@^16.0.0/dist/primer.css" rel="stylesheet" />
+    <link rel="icon" href="/favicon.svg">
+    <style>
+    	.markdown-body {
+    		box-sizing: border-box;
+    		min-width: 200px;
+    		max-width: 980px;
+    		margin: 0 auto;
+    		padding: 45px;
+    	}
+    	@media (max-width: 767px) {
+    		.markdown-body {
+    			padding: 15px;
+    		}
+    	}
+    </style>
+  </head>
   <body>
     ${index}
     <article class="markdown-body">
-    {title && <h1 id="title">${title}</h1>}
-    ${html}
+      {title && <h1 id="title">${title}</h1>}
+      ${html}
     </article>
   </body>
-</html>
-`;
+</html>`;
 };
 
 /* Step 4: Build pages into .html files with appropriate paths */
