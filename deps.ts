dffbfaf122b11969a69ecaa23525c95944c39c28
Gadzhi Kharkharov <me@kkga.me>
2022-01-25 12:10:20 +0300

    use highlightjs

--- deps.ts
+++ deps.ts
@@ -2,8 +2,7 @@ export { default as frontMatter } from "https://esm.sh/front-matter@4.0.2";
 export { slugify } from "https://deno.land/x/slugify/mod.ts";
 export { default as marked } from "https://esm.sh/marked@3.0.7";
 export { escape as htmlEscape } from "https://esm.sh/he@1.2.0";
-// export { default as prism } from "https://esm.sh/prismjs@1.26.0";
-export { default as prism } from "https://cdn.skypack.dev/prismjs";
+export { default as hljs } from "https://cdn.skypack.dev/highlight.js";
 export { default as sanitizeHtml } from "https://esm.sh/sanitize-html@2.6.1";
 
 export {
--- render.ts
+++ render.ts
@@ -1,54 +1,115 @@
-import { htmlEscape, marked, prism } from "./deps.ts";
+import { hljs, htmlEscape, marked, sanitizeHtml } from "./deps.ts";
 import { Heading } from "./main.ts";
 import { parseURL } from "https://unpkg.com/ufo/dist/index.mjs";
+
+const renderer = new marked.Renderer();
+
+const isExternalLink = (href: string): boolean => {
+  const { protocol } = parseURL(href);
+  return protocol !== undefined;
+};
+
+const allowedTags = sanitizeHtml.defaults.allowedTags.concat([
+  "img",
+  "video",
+  "svg",
+  "path",
+  "iframe",
+]);
+
+const sanitize = (html: string) => {
+  return sanitizeHtml(html, {
+    allowedTags,
+    allowedAttributes: {
+      ...sanitizeHtml.defaults.allowedAttributes,
+      img: ["src", "alt", "height", "width", "align"],
+      video: [
+        "src",
+        "alt",
+        "height",
+        "width",
+        "autoplay",
+        "muted",
+        "loop",
+        "playsinline",
+      ],
+      a: ["id", "aria-hidden", "href", "tabindex", "rel"],
+      svg: ["viewbox", "width", "height", "aria-hidden"],
+      path: ["fill-rule", "d"],
+      h1: ["id"],
+      h2: ["id"],
+      h3: ["id"],
+      h4: ["id"],
+      h5: ["id"],
+      h6: ["id"],
+      iframe: ["src", "width", "height"], // Only used when iframe tags are allowed in the first place.
+    },
+    allowedClasses: {
+      div: ["hljs"],
+      span: [
+        "token",
+        "keyword",
+        "operator",
+        "number",
+        "boolean",
+        "function",
+        "string",
+        "comment",
+        "class-name",
+        "regex",
+        "regex-delimiter",
+        "tag",
+        "attr-name",
+        "punctuation",
+        "script-punctuation",
+        "script",
+        "plain-text",
+        "property",
+      ],
+      a: ["anchor"],
+      svg: ["octicon", "octicon-link"],
+    },
+    allowProtocolRelative: false,
+  });
+};
 
 export function render(text: string): [string, Array<string>, Array<Heading>] {
   const links: Array<string> = [];
   const headings: Array<Heading> = [];
 
-  const isExternalLink = (href: string): boolean => {
-    const { protocol } = parseURL(href);
-    return protocol !== undefined;
+  renderer.heading = function (
+    text: string,
+    level: 1 | 2 | 3 | 4 | 5 | 6,
+    raw: string,
+    slugger: marked.Slugger,
+  ): string {
+    const slug = slugger.slug(raw);
+    headings.push({ text, level, slug });
+    return `<h${level} id="${slug}">${text}<a href="#${slug}"></a></h${level}>`;
   };
 
-  const renderer: Partial<marked.Renderer> = {
-    heading(
-      text: string,
-      level: 1 | 2 | 3 | 4 | 5 | 6,
-      raw: string,
-      slugger: marked.Slugger,
-    ): string {
-      const slug = slugger.slug(raw);
-      headings.push({ text, level, slug });
-      return `<h${level} id="${slug}">${text}<a href="#${slug}"></a></h${level}>`;
-    },
-
-    link(href: string, title: string, text: string) {
-      if (isExternalLink(href)) {
-        return `<a href="${href}" rel="external noopener noreferrer" ${
-          title ? "title=${title}" : ""
-        }">${text}</a>`;
-      } else if (href.endsWith(".md")) {
-        links.push(href);
-        const localHref = "/" + href.replace(/\.md$/i, "");
-        return `<a href="${localHref}" ${
-          title ? "title=${title}" : ""
-        }">${text}</a>`;
-      }
-      return `<a href="${href}" ${title ? "title=${title}" : ""}">${text}</a>`;
-    },
+  renderer.link = function (href: string, title: string, text: string) {
+    if (isExternalLink(href)) {
+      return `<a href="${href}" rel="external noopener noreferrer" ${
+        title ? "title=${title}" : ""
+      }">${text}</a>`;
+    } else if (href.endsWith(".md")) {
+      links.push(href);
+      const localHref = "/" + href.replace(/\.md$/i, "");
+      return `<a href="${localHref}" ${
+        title ? "title=${title}" : ""
+      }">${text}</a>`;
+    }
+    return `<a href="${href}" ${title ? "title=${title}" : ""}">${text}</a>`;
   };
 
   marked.use({
     renderer,
     highlight: function (code, lang) {
-      if (prism.languages[lang]) {
-        return prism.highlight(code, prism.languages[lang], lang);
-      } else {
-        return htmlEscape(code);
-      }
+      const language = hljs.getLanguage(lang) ? lang : "plaintext";
+      return hljs.highlight(code, { language }).value;
     },
-    langPrefix: "prism-code language-",
+    langPrefix: "hljs language-",
     baseUrl: "/",
     pedantic: false,
     gfm: true,
@@ -59,5 +120,7 @@ export function render(text: string): [string, Array<string>, Array<Heading>] {
     xhtml: false,
   });
 
-  return [marked.parse(text), links, headings];
+  const html = marked.parse(text);
+
+  return [html, links, headings];
 }
