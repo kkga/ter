b2aa5f4ce829084fd53d97a94d2d97445f77f62c
Gadzhi Kharkharov <me@kkga.me>
2022-04-02 21:36:47 +0300

    bump dependencies

--- deps.ts
+++ deps.ts
@@ -1,16 +1,16 @@
-export * as fs from "https://deno.land/std@0.126.0/fs/mod.ts";
-export * as path from "https://deno.land/std@0.126.0/path/mod.ts";
-export { parse as flagsParse } from "https://deno.land/std@0.126.0/flags/mod.ts";
-export type { Args } from "https://deno.land/std@0.126.0/flags/mod.ts";
+export * as fs from "https://deno.land/std@0.133.0/fs/mod.ts";
+export * as path from "https://deno.land/std@0.133.0/path/mod.ts";
+export { parse as flagsParse } from "https://deno.land/std@0.133.0/flags/mod.ts";
+export type { Args } from "https://deno.land/std@0.133.0/flags/mod.ts";
 export {
   parse as yamlParse,
   stringify as yamlStringify,
-} from "https://deno.land/std@0.126.0/encoding/yaml.ts";
+} from "https://deno.land/std@0.133.0/encoding/yaml.ts";
 export {
   readableStreamFromReader,
   writableStreamFromWriter,
-} from "https://deno.land/std@0.126.0/streams/mod.ts";
-export { serve } from "https://deno.land/std@0.126.0/http/server.ts";
+} from "https://deno.land/std@0.133.0/streams/mod.ts";
+export { serve } from "https://deno.land/std@0.133.0/http/server.ts";
 
 export { default as fm } from "https://esm.sh/front-matter";
 export { default as slugify } from "https://esm.sh/slugify";
