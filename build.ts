b8e2be62f00ee1e23e47788a7436674c087cf641
Gadzhi Kharkharov <me@kkga.me>
2022-05-08 22:15:03 +0300

    use Page[] for index list

--- build.ts
+++ build.ts
@@ -14,17 +14,6 @@ interface Breadcrumb {
   isTag?: boolean;
 }
 
-interface IndexItem {
-  url: string;
-  title: string;
-  description?: string;
-  html?: string;
-  isIndexPage: boolean;
-  pinned: boolean;
-  date: Date | null;
-  readableDate: string | null;
-}
-
 interface PageOpts {
   headInclude: string;
   includeRefresh: boolean;
@@ -44,44 +33,14 @@ interface TagPageOpts {
   siteConf: SiteConfig;
 }
 
-const toReadableDate = (date: Date) =>
-  new Date(date).toLocaleDateString("en-us", {
-    year: "numeric",
-    month: "short",
-    day: "2-digit",
-  });
-
-function generateIndexItems(pages: Array<Page>): Array<IndexItem> {
-  const items: Array<IndexItem> = [];
-
-  for (const p of pages) {
-    const isPinned = hasKey(p.attrs, ["pinned"]);
-    const readableDate = p.date ? toReadableDate(p.date) : null;
-    items.push({
-      url: p.url.pathname,
-      title: p.title,
-      description: p.description,
-      html: p.html,
-      date: p.date ? p.date : null,
-      readableDate,
-      isIndexPage: p.isIndex,
-      pinned: isPinned,
-    });
-  }
-
-  items
+const sortPages = (pages: Page[]): Page[] =>
+  pages
     .sort((a, b) => {
-      if (a.date && b.date) {
-        return b.date.valueOf() - a.date.valueOf();
-      } else {
-        return 0;
-      }
+      if (a.date && b.date) return b.date.valueOf() - a.date.valueOf();
+      else return 0;
     })
-    .sort((a) => a.isIndexPage ? -1 : 0)
-    .sort((a) => a.pinned ? -1 : 0);
-
-  return items;
-}
+    .sort((page) => page.isIndex ? -1 : 0)
+    .sort((page) => hasKey(page.attrs, ["pinned"]) ? -1 : 0);
 
 function generateBreadcrumbs(
   currentPage: Page,
@@ -122,39 +81,30 @@ export async function buildPage(
   page: Page,
   opts: PageOpts,
 ): Promise<string | void> {
-  const { title, description, tags, date, html: content } = page;
-  const readableDate = date ? toReadableDate(date) : null;
   const breadcrumbs = generateBreadcrumbs(page, opts.siteConf.rootName);
-  const backlinkPages = generateIndexItems(opts.backlinkPages);
-  const childPages = generateIndexItems(opts.childPages);
+  const backlinkPages = sortPages(opts.backlinkPages);
+  const childPages = sortPages(opts.childPages);
   const useLogLayout = hasKey(page.attrs, ["log"]) && page.attrs?.log === true;
+  const taggedPages: { [tag: string]: Array<Page> } = {};
 
-  const tagLists: { [tag: string]: Array<IndexItem> } = {};
   for (const tag of Object.keys(opts.taggedPages)) {
-    const tagIndex = generateIndexItems(
+    const tagIndex = sortPages(
       opts.taggedPages[tag].filter((taggedPage) => taggedPage !== page),
     );
     if (tagIndex.length !== 0) {
-      tagLists[tag] = tagIndex;
+      taggedPages[tag] = tagIndex;
     }
   }
 
   eta.templates.define("head", eta.compile(opts.headInclude));
 
   return await eta.renderFile(opts.viewPath, {
-    page: {
-      title,
-      description,
-      content,
-      date: date,
-      tags,
-      readableDate: readableDate,
-    },
+    page,
     indexLayout: useLogLayout ? "log" : "default",
     breadcrumbs,
     childPages,
     backlinkPages,
-    taggedIndexLinks: tagLists,
+    taggedIndexLinks: taggedPages,
     childTags: opts.childTags,
     site: opts.siteConf,
     includeRefresh: opts.includeRefresh,
@@ -166,7 +116,7 @@ export async function buildTagPage(
   opts: TagPageOpts,
 ): Promise<string | void> {
   eta.templates.define("head", eta.compile(opts.headInclude));
-  const indexItems = generateIndexItems(opts.taggedPages);
+  const indexItems = sortPages(opts.taggedPages);
   const breadcrumbs: Array<Breadcrumb> = [
     { slug: "index", url: "/", current: false },
     { slug: `#${tagName}`, url: "", current: true, isTag: true },
