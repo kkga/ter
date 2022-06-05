eaabecaf33fad407d8b06816879be54f2b6e64cf
Gadzhi Kharkharov <me@kkga.me>
2022-06-05 19:18:41 +0300

    update deno deps

--- deps.ts
+++ deps.ts
@@ -1,16 +1,16 @@
-export * as fs from "https://deno.land/std@0.137.0/fs/mod.ts";
-export * as path from "https://deno.land/std@0.137.0/path/mod.ts";
-export { parse as flagsParse } from "https://deno.land/std@0.137.0/flags/mod.ts";
-export type { Args } from "https://deno.land/std@0.137.0/flags/mod.ts";
+export * as fs from "https://deno.land/std@0.142.0/fs/mod.ts";
+export * as path from "https://deno.land/std@0.142.0/path/mod.ts";
+export { parse as flagsParse } from "https://deno.land/std@0.142.0/flags/mod.ts";
+export type { Args } from "https://deno.land/std@0.142.0/flags/mod.ts";
 export {
   parse as yamlParse,
   stringify as yamlStringify,
-} from "https://deno.land/std@0.137.0/encoding/yaml.ts";
+} from "https://deno.land/std@0.142.0/encoding/yaml.ts";
 export {
   readableStreamFromReader,
   writableStreamFromWriter,
-} from "https://deno.land/std@0.137.0/streams/mod.ts";
-export { serve } from "https://deno.land/std@0.137.0/http/server.ts";
+} from "https://deno.land/std@0.142.0/streams/mod.ts";
+export { serve } from "https://deno.land/std@0.142.0/http/server.ts";
 
 export { default as fm } from "https://esm.sh/front-matter";
 export { default as slugify } from "https://esm.sh/slugify";
