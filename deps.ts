2e0f2533ab94cbee2b898e111f66f370d6fbb0df
Gadzhi Kharkharov <me@kkga.me>
2022-10-03 02:29:51 +0300

    cleanup deps

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
+// std
 export { parse as parseFlags } from "https://deno.land/std@0.158.0/flags/mod.ts";
-
 export {
   copy,
   emptyDir,
@@ -8,7 +8,6 @@ export {
   walk,
 } from "https://deno.land/std@0.158.0/fs/mod.ts";
 export type { WalkEntry } from "https://deno.land/std@0.158.0/fs/mod.ts";
-
 export {
   basename,
   common,
@@ -19,12 +18,11 @@ export {
   relative,
   toFileUrl,
 } from "https://deno.land/std@0.158.0/path/mod.ts";
-
 export { readableStreamFromReader } from "https://deno.land/std@0.158.0/streams/mod.ts";
-
 export { serve } from "https://deno.land/std@0.158.0/http/server.ts";
-
 export * as frontmatter from "https://deno.land/std@0.158.0/encoding/front_matter.ts";
+
+// 3
 export {
   normalizeURL,
   parseURL,
@@ -34,17 +32,8 @@ export {
   withTrailingSlash,
 } from "https://esm.sh/ufo@0.8.5/";
 export type { ParsedURL } from "https://esm.sh/ufo@0.8.5/";
-
-export {
-  compile,
-  configure,
-  render,
-  templates,
-} from "https://deno.land/x/eta@v1.12.3/mod.ts";
-
+export * as eta from "https://deno.land/x/eta@v1.12.3/mod.ts";
 export { deepmerge } from "https://deno.land/x/deepmergets@v4.2.2/dist/deno/index.ts";
 export { marked } from "https://esm.sh/marked@4.1.0/";
 export { default as slugify } from "https://esm.sh/slugify@1.6.5";
-export { minify } from "https://esm.sh/html-minifier-terser@7.0.0/";
-export type { Options as MinifyOpts } from "https://esm.sh/html-minifier-terser@7.0.0/";
 export { default as hljs } from "https://cdn.skypack.dev/highlight.js";
