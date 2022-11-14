ae3b607e878b94cf50c7fa966b6c1dfbcd3d59a8
Gadzhi Kharkharov <me@kkga.me>
2022-11-14 11:49:57 +0300

    update deps

--- deps.ts
+++ deps.ts
@@ -23,6 +23,11 @@ export {
   parse as flagsParse,
 } from "https://deno.land/std@0.164.0/flags/mod.ts";
 
+export {
+  extract as fmExtract,
+  test as fmTest,
+} from "https://deno.land/std@0.164.0/encoding/front_matter.ts";
+
 export { serve as httpServe } from "https://deno.land/std@0.164.0/http/server.ts";
 
 export {
@@ -33,9 +38,14 @@ export {
   withoutTrailingSlash,
 } from "https://esm.sh/ufo@0.8.6/";
 
-// export { default as preact, h } from "https://esm.sh/preact@10.11.2^";
+export { Feed } from "https://esm.sh/feed@4.2.2";
+export { deepmerge } from "https://deno.land/x/deepmergets@v4.2.2/dist/deno/index.ts";
+export { default as slugify } from "https://esm.sh/slugify@1.6.5/";
+
 export { renderToString } from "https://esm.sh/preact-render-to-string@5.2.6^";
+
 export { marked } from "https://esm.sh/marked@4.2.2/";
+
 export {
   apply,
   type Configuration as TwindConfiguration,
@@ -47,11 +57,3 @@ export {
   virtualSheet,
 } from "https://esm.sh/twind@0.16.17^/sheets";
 export { css } from "https://esm.sh/twind@0.16.17^/css";
-
-export { Feed } from "https://esm.sh/feed@4.2.2";
-export { deepmerge } from "https://deno.land/x/deepmergets@v4.2.2/dist/deno/index.ts";
-export { default as slugify } from "https://esm.sh/slugify@1.6.5/";
-export {
-  extract as fmExtract,
-  test as fmTest,
-} from "https://deno.land/std/encoding/front_matter.ts";
