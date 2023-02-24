d2a7d2fbcf1b7f83d71779cd15670c28cd0537b6
Gadzhi Kharkharov <me@kkga.me>
2023-02-25 01:15:37 +0300

    update deps

--- deps.ts
+++ deps.ts
@@ -1,15 +1,15 @@
 export {
   extract as fmExtract,
   test as fmTest,
-} from "https://deno.land/std@0.173.0/encoding/front_matter.ts";
-export { parse as flagsParse } from "https://deno.land/std@0.173.0/flags/mod.ts";
+} from "https://deno.land/std@0.178.0/encoding/front_matter/any.ts";
+export { parse as flagsParse } from "https://deno.land/std@0.178.0/flags/mod.ts";
 export {
   emptyDirSync,
   ensureDirSync,
   walk,
   type WalkEntry,
-} from "https://deno.land/std@0.173.0/fs/mod.ts";
-export { serve as httpServe } from "https://deno.land/std@0.173.0/http/server.ts";
+} from "https://deno.land/std@0.178.0/fs/mod.ts";
+export { serve as httpServe } from "https://deno.land/std@0.178.0/http/server.ts";
 export {
   basename,
   common,
@@ -18,9 +18,10 @@ export {
   isAbsolute,
   join,
   relative,
-} from "https://deno.land/std@0.173.0/path/mod.ts";
-export { readableStreamFromReader } from "https://deno.land/std@0.173.0/streams/mod.ts";
-export { deepmerge } from "https://deno.land/x/deepmergets@v4.2.2/dist/deno/index.ts";
+} from "https://deno.land/std@0.178.0/path/mod.ts";
+export { readableStreamFromReader } from "https://deno.land/std@0.178.0/streams/mod.ts";
+export { deepmerge } from "https://deno.land/x/deepmergets@v4.3.0/dist/deno/index.ts";
+export { slug as slugify } from "https://deno.land/x/slug@v1.1.0/mod.ts";
 export {
   apply,
   css,
@@ -35,27 +36,26 @@ export {
   twind,
   tx,
   virtual,
-} from "npm:@twind/core@1.1.2";
-export { default as presetAutoprefix } from "npm:@twind/preset-autoprefix@1.0.6";
-export { default as presetExt } from "npm:@twind/preset-ext@1.0.6";
+} from "npm:@twind/core@1.1.3";
+export { default as presetAutoprefix } from "npm:@twind/preset-autoprefix@1.0.7";
+export { default as presetExt } from "npm:@twind/preset-ext@1.0.7";
 export {
   mauve as neutral,
   mauveDark as neutralDark,
   violet as accent,
   violetDark as accentDark,
-} from "npm:@twind/preset-radix-ui@1.0.6/colors";
-export { default as darkColor } from "npm:@twind/preset-radix-ui@1.0.6/darkColor";
-export { default as presetTailwind } from "npm:@twind/preset-tailwind@1.1.3/base";
-export { default as presetTypography } from "npm:@twind/preset-typography@1.0.6";
+} from "npm:@twind/preset-radix-ui@1.0.7/colors";
+export { default as darkColor } from "npm:@twind/preset-radix-ui@1.0.7/darkColor";
+export { default as presetTailwind } from "npm:@twind/preset-tailwind@1.1.4/base";
+export { default as presetTypography } from "npm:@twind/preset-typography@1.0.7";
 export { Feed } from "npm:feed@4.2.2";
 export { default as hljs } from "npm:highlight.js@11.7.0";
 export { marked } from "npm:marked@4.2.12";
 export { renderToString } from "npm:preact-render-to-string@5.2.6";
-export { default as slugify } from "npm:slugify@1.6.5";
 export {
   parseURL,
   withLeadingSlash,
   withoutLeadingSlash,
   withoutTrailingSlash,
   type ParsedURL,
-} from "npm:ufo@1.0.1";
+} from "npm:ufo@1.1.0";
