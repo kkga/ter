51bdc6263740072e329a9e295910a34381f455bd
Gadzhi Kharkharov <me@kkga.me>
2023-04-19 22:32:14 +0400

    use npm specifiers

--- deps.ts
+++ deps.ts
@@ -37,26 +37,26 @@ export {
   twind,
   tx,
   virtual,
-} from "https://esm.sh/@twind/core@1.1.3";
-export { default as presetAutoprefix } from "https://esm.sh/@twind/preset-autoprefix@1.0.7";
-export { default as presetExt } from "https://esm.sh/@twind/preset-ext@1.0.7";
+} from "npm:@twind/core@1.1.3";
+export { default as presetAutoprefix } from "npm:@twind/preset-autoprefix@1.0.7";
+export { default as presetExt } from "npm:@twind/preset-ext@1.0.7";
 export {
   mauve as neutral,
   mauveDark as neutralDark,
   violet as accent,
   violetDark as accentDark,
-} from "https://esm.sh/@twind/preset-radix-ui@1.0.7/colors";
-export { default as darkColor } from "https://esm.sh/@twind/preset-radix-ui@1.0.7/darkColor";
-export { default as presetTailwind } from "https://esm.sh/@twind/preset-tailwind@1.1.4/base";
-export { default as presetTypography } from "https://esm.sh/@twind/preset-typography@1.0.7";
-export { Feed } from "https://esm.sh/feed@4.2.2";
-export { default as hljs } from "https://esm.sh/highlight.js@11.7.0";
-export { marked } from "https://esm.sh/marked@4.3.0";
-export { renderToString } from "https://esm.sh/preact-render-to-string@5.2.6";
+} from "npm:@twind/preset-radix-ui@1.0.7/colors";
+export { default as darkColor } from "npm:@twind/preset-radix-ui@1.0.7/darkColor";
+export { default as presetTailwind } from "npm:@twind/preset-tailwind@1.1.4/base";
+export { default as presetTypography } from "npm:@twind/preset-typography@1.0.7";
+export { Feed } from "npm:feed@4.2.2";
+export { default as hljs } from "npm:highlight.js@11.7.0";
+export { marked } from "npm:marked@4.3.0";
+export { renderToString } from "npm:preact-render-to-string@5.2.6";
 export {
   parseURL,
   withLeadingSlash,
   withoutLeadingSlash,
   withoutTrailingSlash,
   type ParsedURL,
-} from "https://esm.sh/ufo@1.1.1";
+} from "npm:ufo@1.1.1";
