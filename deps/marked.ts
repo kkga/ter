3531b3adcb0d0a4bce5e561187f8759ba58892b6
Gadzhi Kharkharov <me@kkga.me>
2023-09-29 13:56:44 +0300

    use named exports

--- deps/marked.ts
+++ deps/marked.ts
@@ -1,2 +1,2 @@
-export { marked } from "npm:marked@9.0.3";
+export { marked, type Renderer } from "npm:marked@9.0.3";
 export * as admonition from "npm:marked-admonition-extension@0.0.4";
--- deps/std.ts
+++ deps/std.ts
@@ -1,26 +1,5 @@
-export { parse as flagsParse } from "https://deno.land/std@0.203.0/flags/mod.ts";
-
-export {
-  extract as fmExtract,
-  test as fmTest,
-} from "https://deno.land/std@0.203.0/front_matter/any.ts";
-
-export {
-  emptyDirSync,
-  ensureDir,
-  ensureDirSync,
-  walk,
-  type WalkEntry,
-} from "https://deno.land/std@0.203.0/fs/mod.ts";
-
-export {
-  basename,
-  common,
-  dirname,
-  extname,
-  isAbsolute,
-  join,
-  relative,
-} from "https://deno.land/std@0.203.0/path/mod.ts";
-
+export * as flags from "https://deno.land/std@0.203.0/flags/mod.ts";
+export * as fm from "https://deno.land/std@0.203.0/front_matter/any.ts";
+export * as fs from "https://deno.land/std@0.203.0/fs/mod.ts";
+export * as path from "https://deno.land/std@0.203.0/path/mod.ts";
 export { assertEquals } from "https://deno.land/std@0.203.0/assert/mod.ts";
